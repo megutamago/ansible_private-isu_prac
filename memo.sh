64d7dea02c2db654d1a526204a4122c718a8c47c
megutamago <naoya05280708@gmail.com>
2023-04-20 23:01:12 +0900

    submit

--- memo.sh
+++ memo.sh
@@ -1,28 +1,3 @@
-#
-#git clone https://github.com/catatsuy/private-isu.git
-#cd ./private-isu/provisioning
-#
-## ALL isu
-#cp /home/user/.ssh/authorized_keys /root/.ssh
-#
-#vim hosts
-#ansible-playbook -i hosts image/ansible/playbooks.yml --skip-tags nodejs
-#ansible-playbook -i hosts bench/ansible/playbooks.yml
-#
-## isu-bm
-#mv private_isu.git private_isu
-#
-#su - isucon
-#/home/isucon/private_isu/benchmarker/bin/benchmarker -u /home/isucon/private_isu/benchmarker/userdata -t http://192.168.11.41
-
-
-ansible-playbook -i hosts image/ansible/playbooks.yml --skip-tags nodejs
-ansible-playbook -i hosts bench/ansible/playbooks.yml
-# isu-bm
-su - isucon
-/home/isucon/private_isu.git/benchmarker/bin/benchmarker -u /home/isucon/private_isu.git/benchmarker/userdata -t http://192.168.11.41
-
-
 ### ssh port foward ###
 ssh -fN -L 0.0.0.0:8080:192.168.11.41:80 192.168.11.44
 
@@ -44,11 +19,12 @@ use mysql;
 SHOW TABLES;
 
 
-
+### mysqldump ###
 sudo apt -y install pv # progressbar
 mysqldump -u isuconp -pisuconp -h localhost isuconp | gzip > hoge.dump.sql.gz
 gzip -d db_name.dump.sql.gz
 mysql -u isuconp -pisuconp -h localhost isuconp < hoge.dump.sql
+# 実績　↑４つ
 
 mysqldump -u isuconp -pisuconp -h localhost isuconp | gzip | pv --progress db_name.dump.sql(or --size 10g) -t -e -r -a > hoge.dump.sql.gz
 
@@ -64,3 +40,7 @@ gzip -d db_name.dump.sql.gz | pv --progress --size 1.4g -t -e -r -a
 # recovery
 pv hoge.dump.sql.gz --progress -t -e -r -a | mysql -u isuconp -pisuconp -h localhost isuconp < hoge.dump.sql
 pv db_name.dump.sql | mysql -u root -pPASSWORD < testdatabase
+
+
+#alp
+cat /var/log/nginx/access.log | alp ltsv -m '/image/[0-9]+,/posts/[0-9]+'
