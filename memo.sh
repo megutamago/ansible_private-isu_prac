d629fe5d4a70a2c6facc7aac8414bc059d86dada
megutamago <naoya05280708@gmail.com>
2023-04-23 03:37:19 +0900

    submit

--- memo.sh
+++ memo.sh
@@ -1,33 +1,51 @@
 ### ssh port foward ###
+
+#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\
 ssh -fN -L 0.0.0.0:8080:192.168.11.41:80 192.168.11.44
-
-# pprof
 ssh -fN -L 0.0.0.0:1080:localhost:1080 192.168.11.41
-#ssh -fN -L 0.0.0.0:6060:localhost:6060 192.168.11.41
+#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\
 
-ssh -fN -L 0.0.0.0:8080:192.168.11.41:80 192.168.11.41
-ssh -fN -L 0.0.0.0:9100:192.168.11.41:9100 192.168.11.41
-ssh -fN -L 0.0.0.0:3100:192.168.11.41:3100 192.168.11.41
-ssh -fN -L 0.0.0.0:3200:192.168.11.41:3200 192.168.11.41
+ssh -fN -L 0.0.0.0:8080:192.168.11.41:80 192.168.11.44  # test port trans
+ssh -fN -L 0.0.0.0:19999:192.168.11.41:19999 192.168.11.44  # netdata
+ssh -fN -L 0.0.0.0:1080:localhost:1080 192.168.11.41  # pprof
+ssh -fN -L 0.0.0.0:9100:192.168.11.41:9100 192.168.11.41  # node_exporter
+ssh -fN -L 0.0.0.0:3100:192.168.11.41:3100 192.168.11.41  # fluent-bit
 ps aux | grep ssh
-curl localhost:8080
-#curl http://localhost:6060/debug/pprof/
-## http://192.168.11.31:6060/debug/pprof/
+
+# grafana port 変換
+curl http://localhost:8080/
+# pprof
 curl http://127.0.0.1:1080/
 # http://192.168.11.31:1080/
 kill <PID>
 
+### wipe ps
+kill $(ps aux | grep 'ssh -fN -L 0.0.0.0:8080:192.168.11.41:80 192.168.11.44' | awk '{print $2}' | sed '$d')
+kill $(ps aux | grep 'ssh -fN -L 0.0.0.0:19999:192.168.11.41:19999 192.168.11.44' | awk '{print $2}' | sed '$d')
+kill $(ps aux | grep 'ssh -fN -L 0.0.0.0:1080:localhost:1080 192.168.11.41' | awk '{print $2}' | sed '$d')
+kill $(ps aux | grep 'ssh -fN -L 0.0.0.0:9100:192.168.11.41:9100 192.168.11.41' | awk '{print $2}' | sed '$d')
+kill $(ps aux | grep 'ssh -fN -L 0.0.0.0:3100:192.168.11.41:3100 192.168.11.41' | awk '{print $2}' | sed '$d')
+ps aux | grep ssh
 
-# pprof
+
+
+
+##### pprof #####
+
+# Graph requirements ?????
+apt install -y graphviz
+
 # trace
 curl -o trace.out http://localhost:6060/debug/pprof/trace?seconds=60
-go tool trace trace.out
+go tool trace -http=localhost:1080 trace.out
 # profile
 #\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\
 curl -o cpu.pprof http://localhost:6060/debug/pprof/profile?seconds=60
 go tool pprof -http=localhost:1080 cpu.pprof
 http://192.168.11.31:1080/
 #\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\#\
+
+
 
 
 ### mysql ###
@@ -37,6 +55,7 @@ SHOW DATABASES;
 use isuconp;
 use mysql;
 SHOW TABLES;
+
 
 
 ### mysqldump ###
@@ -62,11 +81,14 @@ pv hoge.dump.sql.gz --progress -t -e -r -a | mysql -u isuconp -pisuconp -h local
 pv db_name.dump.sql | mysql -u root -pPASSWORD < testdatabase
 
 
-#alp
+
+# alp
 cat /var/log/nginx/access.log | alp ltsv -m '/image/[0-9]+,/posts/[0-9]+' --sort avg -r -o count,1xx,2xx,3xx,4xx,5xx,min,max,avg,sum,p99,method,uri
 
 
-#systemd
+
+##### systemd #####
+
 ※./files/pattern.sh
 systemctl list-units --all --type=service | grep -v inactive > systemd-extract.txt 
 systemctl list-units --all --type=service | grep -v inactive \
@@ -77,13 +99,9 @@ grep -vf pattern.txt systemd-extract.txt | tee r.txt
 grep -vf pattern.txt systemd-extract.txt | awk '{print $1}'
 
 
-# unarchive
+
+##### unarchive ##### 
 rm -rf ../test-private_isu/webapp/ && \
 tar zxvfp ./files/fetch/webapp.tar.gz -C ../test-private_isu/ && \
 mv ../test-private_isu/home/isucon/private_isu/webapp ../test-private_isu/ && \
 rm -rf ../test-private_isu/home/
-
-
-
-go tool pprof -http=0.0.0.0:1080 ./hoge  http://localhost:6060/debug/pprof/profile
-curl -s http://0.0.0.0:1080/debug/pprof/profile > cpu.pprof
